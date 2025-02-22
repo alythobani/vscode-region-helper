2735a53f3fc8b46a189ead3c5ed2ad6d8b0cf174
Aly Thobani <athobani9@gmail.com>
2025-02-21 16:40:35 -0800

    docs: add docstrings for API

--- src/extension.ts
+++ src/extension.ts
@@ -16,12 +16,23 @@ import { RegionTreeViewProvider } from "./treeView/RegionTreeViewProvider";
 import { goToRegionTreeItem, goToRegionTreeItemCommandId } from "./treeView/goToRegionTreeItem";
 
 export type RegionHelperAPI = {
+  /** Returns an up-to-date list of top-level regions in the current active editor. This is used to
+   * render the tree view, for example. The list will be empty if no editor is active. */
   getTopLevelRegions(): Region[];
+  /** Returns an up-to-date flat list of all regions in the current active editor, each of which has
+   * an extra `flatRegionIdx` field on it. This is used when navigating to previous/next regions,
+   * for example.  The list will be empty if no editor is active. */
   getFlattenedRegions(): FlattenedRegion[];
+  /** Returns the currently active region in the current active editor, if any. */
   getActiveRegion(): Region | undefined;
+  /** Returns an up-to-date list of invalid markers in the current active editor. The list will be
+   * empty if no editor is active. */
   getInvalidMarkers(): InvalidMarker[];
+  /** An event that fires when the list of regions in the current active editor changes. */
   onDidChangeRegions: vscode.Event<void>;
+  /** An event that fires when the active region in the current active editor changes. */
   onDidChangeActiveRegion: vscode.Event<void>;
+  /** An event that fires when the list of invalid markers in the current active editor changes. */
   onDidChangeInvalidMarkers: vscode.Event<void>;
 };
 
