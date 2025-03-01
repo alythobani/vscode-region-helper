f1dcefd1bb9ac41a545122dec0eedad0782e2ef3
Aly Thobani <athobani9@gmail.com>
2025-02-28 16:28:02 -0800

    refactor: bring out generateFullTree helper function

--- src/treeView/fullTreeView/mergeRegionAndSymbolTreeItems.ts
+++ src/treeView/fullTreeView/mergeRegionAndSymbolTreeItems.ts
@@ -7,15 +7,63 @@ export function mergeRegionsAndSymbols({
   flattenedRegionItems: FullTreeItem[];
   flattenedSymbolItems: FullTreeItem[];
 }): FullTreeItem[] {
-  const sortedFullTreeItems = mergeFlattenedRegionAndSymbolTreeItems({
+  const flattenedFullTreeItems = mergeFlattenedRegionAndSymbolTreeItems({
     flattenedRegionItems,
     flattenedSymbolItems,
   });
+  return generateFullTree({ flattenedFullTreeItems });
+}
 
+/**
+ * Merges the flattened (and sorted within themselves) region and symbol tree items into a single
+ * sorted list in O(n).
+ */
+function mergeFlattenedRegionAndSymbolTreeItems({
+  flattenedRegionItems,
+  flattenedSymbolItems,
+}: {
+  flattenedRegionItems: FullTreeItem[];
+  flattenedSymbolItems: FullTreeItem[];
+}): FullTreeItem[] {
+  const flattenedFullTreeItems: FullTreeItem[] = [];
+  for (
+    let regionIdx = 0,
+      symbolIdx = 0,
+      regionItem = flattenedRegionItems[regionIdx],
+      symbolItem = flattenedSymbolItems[symbolIdx];
+    regionItem !== undefined || symbolItem !== undefined;
+    regionItem = flattenedRegionItems[regionIdx], symbolItem = flattenedSymbolItems[symbolIdx]
+  ) {
+    if (regionItem === undefined) {
+      // Append the remaining symbol items
+      flattenedFullTreeItems.push(...flattenedSymbolItems.slice(symbolIdx));
+      break;
+    }
+    if (symbolItem === undefined) {
+      // Append the remaining region items
+      flattenedFullTreeItems.push(...flattenedRegionItems.slice(regionIdx));
+      break;
+    }
+    if (regionItem.range.start.isBefore(symbolItem.range.start)) {
+      flattenedFullTreeItems.push(regionItem);
+      regionIdx++;
+    } else {
+      flattenedFullTreeItems.push(symbolItem);
+      symbolIdx++;
+    }
+  }
+  return flattenedFullTreeItems;
+}
+
+function generateFullTree({
+  flattenedFullTreeItems,
+}: {
+  flattenedFullTreeItems: FullTreeItem[];
+}): FullTreeItem[] {
   const mergedTreeRoots: FullTreeItem[] = [];
   const parentStack: FullTreeItem[] = [];
   // Build the hierarchical tree structure
-  for (const treeItem of sortedFullTreeItems) {
+  for (const treeItem of flattenedFullTreeItems) {
     // Remove parents that are no longer valid (i.e., this item is outside their range)
     let currentParent = parentStack[parentStack.length - 1];
     for (
@@ -37,46 +85,4 @@ export function mergeRegionsAndSymbols({
     parentStack.push(treeItem);
   }
   return mergedTreeRoots;
-}
-
-/**
- * Merges the flattened (and sorted within themselves) region and symbol tree items into a single
- * sorted list in O(n).
- */
-function mergeFlattenedRegionAndSymbolTreeItems({
-  flattenedRegionItems,
-  flattenedSymbolItems,
-}: {
-  flattenedRegionItems: FullTreeItem[];
-  flattenedSymbolItems: FullTreeItem[];
-}): FullTreeItem[] {
-  // Merge the two sorted lists in O(n)
-  const sortedFullTreeItems: FullTreeItem[] = [];
-  for (
-    let regionIdx = 0,
-      symbolIdx = 0,
-      regionItem = flattenedRegionItems[regionIdx],
-      symbolItem = flattenedSymbolItems[symbolIdx];
-    regionItem !== undefined || symbolItem !== undefined;
-    regionItem = flattenedRegionItems[regionIdx], symbolItem = flattenedSymbolItems[symbolIdx]
-  ) {
-    if (regionItem === undefined) {
-      // Append the remaining symbol items
-      sortedFullTreeItems.push(...flattenedSymbolItems.slice(symbolIdx));
-      break;
-    }
-    if (symbolItem === undefined) {
-      // Append the remaining region items
-      sortedFullTreeItems.push(...flattenedRegionItems.slice(regionIdx));
-      break;
-    }
-    if (regionItem.range.start.isBefore(symbolItem.range.start)) {
-      sortedFullTreeItems.push(regionItem);
-      regionIdx++;
-    } else {
-      sortedFullTreeItems.push(symbolItem);
-      symbolIdx++;
-    }
-  }
-  return sortedFullTreeItems;
 }
